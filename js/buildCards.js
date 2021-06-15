da4127e1375a77a6abc45f3ae6cf3de1059d0155
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-06-15 16:27:33 +0200

    Cards class maybe done, needs testing now

--- js/buildCards.js
+++ js/buildCards.js
@@ -1,8 +1,84 @@
-const cardsSymbolArr = []
+const symbols = [
+    {name: "apple", img: "apple.png"},
+    {name: "baguettemagique", img: "baguettemagique.png"},
+    {name: "baguettemagique2", img: "baguettemagique2.png"},
+    {name: "basket", img: "basket.png"},
+    {name: "beachball", img: "beachball.png"},
+    {name: "boat", img: "boat.png"},
+    {name: "book", img: "book.png"},
+    {name: "boots", img: "boots.png"},
+    {name: "branch", img: "branch.png"},
+    {name: "bread", img: "bread.png"},
+    {name: "bucket", img: "bucket.png"},
+    {name: "cake", img: "cake.png"},
+    {name: "camera", img: "camera.png"},
+    {name: "cappuccino", img: "cappuccino.png"},
+    {name: "car", img: "car.png"},
+    {name: "cards", img: "cards.png"},
+    {name: "cauldron", img: "cauldron.png"},
+    {name: "cinematickets", img: "cinematickets.png"},
+    {name: "circus", img: "circus.png"},
+    {name: "clap", img: "clap.png"},
+    {name: "cowskull", img: "cowskull.png"},
+    {name: "crab", img: "crab.png"},
+    {name: "crocodile", img: "crocodile.png"},
+    {name: "croissant", img: "croissant.png"},
+    {name: "dragon", img: "dragon.png"},
+    {name: "droplets", img: "droplets.png"},
+    {name: "elephant", img: "elephant.png"},
+    {name: "football", img: "football.png"},
+    {name: "fraise", img: "fraise.png"},
+    {name: "fries", img: "fries.png"},
+    {name: "giraffe", img: "giraffe.png"},
+    {name: "glasses", img: "glasses.png"},
+    {name: "guitar", img: "guitar.png"},
+    {name: "hat", img: "hat.png"},
+    {name: "jellyfish", img: "jellyfish.png"},
+    {name: "lemon", img: "lemon.png"},
+    {name: "lion", img: "lion.png"},
+    {name: "loupe", img: "loupe.png"},
+    {name: "magicball", img: "magicball.png"},
+    {name: "magichand", img: "magichand.png"},
+    {name: "manege", img: "manege.png"},
+    {name: "map", img: "map.png"},
+    {name: "monkey", img: "monkey.png"},
+    {name: "mountains", img: "mountains.png"},
+    {name: "octopus", img: "octopus.png"},
+    {name: "oldphone", img: "oldphone.png"},
+    {name: "orange", img: "orange.png"},
+    {name: "pancakes", img: "pancakes.png"},
+    {name: "parasol", img: "parasol.png"},
+    {name: "parasol2", img: "parasol2.png"},
+    {name: "pear", img: "pear.png"},
+    {name: "potion", img: "potion.png"},
+    {name: "pride", img: "pride.png"},
+    {name: "retrocar", img: "retrocar.png"},
+    {name: "retroradio", img: "retroradio.png"},
+    {name: "roulotte", img: "roulotte.png"},
+    {name: "scissors", img: "scissors.png"},
+    {name: "shoe", img: "shoe.png"},
+    {name: "skate", img: "skate.png"},
+    {name: "skull", img: "skull.png"},
+    {name: "speaker", img: "speaker.png"},
+    {name: "stopsign", img: "stopsign.png"},
+    {name: "sun", img: "sun.png"},
+    {name: "tape", img: "tape.png"},
+    {name: "tea", img: "tea.png"},
+    {name: "turtle", img: "turtle.png"},
+    {name: "unicorn", img: "unicorn.png"},
+    {name: "valise", img: "valise.png"},
+    {name: "water", img: "water.png"},
+    {name: "wateringcan", img: "wateringcan.png"},
+    {name: "wave", img: "wave.png"},
+    {name: "weights", img: "weights.png"},
+    {name: "whale", img: "whale.png"},
+    {name: "witchhat", img: "witchhat.png"},
+    {name: "wood", img: "wood.png"}
+]
 
 class Cards {
     constructor() {
-        this.array = cardsSymbolArr;
+        this.array = symbols;
         this.name = "";
         this.img = "";
 
@@ -14,41 +90,54 @@ class Cards {
     }
     
 
-    filterArray(compareFirst, compareSecond) {
-        if (compareSecond) {
-            let firstFilter = this.array.filter(symbol => !compareFirst.includes(symbol));
-            this.array = firstFilter.filter(symbol => !compareSecond.includes(symbol));
+    filterArray(compareItself, otherArray) {
+        let filteredArray;
+        if (otherArray) {
+            let firstFilter = this.array.filter(symbol => !otherArray.includes(symbol));
+            filteredArray = firstFilter.filter(symbol => !compareItself.includes(symbol));
         } else {
-            this.array = this.array.filter(symbol => !compareFirst.includes(symbol));
+            filteredArray = this.array.filter(symbol => !compareItself.includes(symbol));
         }
-        return this.array;
+        return filteredArray;
     }
 
 
-    cardArray(cardToBuild, cardAbove) {
 
-        cardToBuild.forEach ((symbol, index) => {
-            let filteredArray = this.filterArray(symbols, cardAbove, cardToBuild);
-            let random = filteredArray[Math.floor(Math.random() * filteredArray.length)];
+    cardArray (cardToBuild, cardAbove) {
 
-            cardToBuild.splice(index, 1, random)
-        })
+        if(!cardAbove) {
+            cardToBuild.forEach ((symbol, index) => {
+                let filteredArray = this.filterArray(cardToBuild);
+                let random = filteredArray[Math.floor(Math.random() * filteredArray.length)];
 
-        let randomCommon = cardAbove[Math.floor(Math.random() * cardAbove.length)];
-        let randomIndex = Math.floor(Math.random() * cardToBuild.length);
+                cardToBuild.splice(index, 1, random)
+            })
 
-        cardToBuild.splice(randomIndex, 0, randomCommon);
+            return cardToBuild;
 
-        return cardToBuild;
+        } else if (cardAbove) {
+            cardToBuild.forEach ((symbol, index) => {
+                let filteredArray = this.filterArray(cardToBuild, cardAbove);
+                let random = filteredArray[Math.floor(Math.random() * filteredArray.length)];
+
+                cardToBuild.splice(index, 1, random)
+            })
+
+            let randomCommon = cardAbove[Math.floor(Math.random() * cardAbove.length)];
+            let randomIndex = Math.floor(Math.random() * cardToBuild.length);
+
+            cardToBuild.splice(randomIndex, 0, randomCommon);
+
+            return cardToBuild;
+        }
+    }
         
-    }
 
 
     buildCard (cardBuilt) {
         let cardInner = ``;
 
-        cardBuilt.forEach((symbol) => {
-            let index = cardBuilt.indexOf(symbol)
+        cardBuilt.forEach((symbol, index) => {
             cardInner += 
             `<li class="symbol ${symbol.name}">
             <button class="play-button">
@@ -56,50 +145,17 @@ class Cards {
             </button>
             </li>`;
         })
-    }
 
-
-    attachCard () {
+        return cardInner;
 
     }
 
-}
 
 
-
-//PLAYER CARD
-
-function playerCardArray() {
-    let filteredArray = symbols.filter(symbol => !mainCard.includes(symbol));
-      
+    attachCard (cardElement, innerResult) {
+        cardElement.innerHTML = innerResult;
+    }
     
-    playerCard.forEach ((symbol, index) => {
-        let random = filteredArray[Math.floor(Math.random() * filteredArray.length)];
 
-        playerCard.splice(index, 1, random)
-
-        filteredArray = filteredArray.filter(el => !mainCard.includes(el) && !playerCard.includes(el))
-    })
-    
-    let randomCommon = mainCard[Math.floor(Math.random() * mainCard.length)];
-    let randomIndex = Math.floor(Math.random() * playerCard.length);
-
-    playerCard.splice(randomIndex, 0, randomCommon);
-
-    console.log(filteredArray.length);
-    return playerCard;
 }
 
-function buildPlayerCard () {
-    playerCard.forEach((symbol) => {
-        let index = playerCard.indexOf(symbol)
-        playerCardInner += 
-        `<li class="player symbol ${symbol.name}">
-        <button class="play-button">
-        <img class="img${index}" src="img/${symbol.img}" alt="${symbol.name}" width="100" height="100">
-        </button>
-        </li>`;
-    })
-    playerCardElement.innerHTML = playerCardInner;
-    //console.log(inner);
-}
